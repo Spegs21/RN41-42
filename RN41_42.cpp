779d28bf2b5e0feb42e9cd99aa73abd3103bc64c
Chris Spegal <Spegs21@gmail.com>
2017-06-12 15:30:22 -0400

    Updated getString(char terminationChar).

--- RN41_42.cpp
+++ RN41_42.cpp
@@ -546,26 +546,27 @@ void RN41_42::sendString(String msg) {
 
 String RN41_42::getString() {
 	String msg = "";
-	char prev = ' ';
-	char curr = ' ';
+	char prev;
+	char curr;
 	while (serial.available()) {
 		prev = curr;
 		curr = serial.read();
 		msg += curr;
+		if (prev == '\r' && curr == '\n') {
+			break;
+		}
 	}
 	return msg;
 }
 
 String RN41_42::getString(char terminationChar) {
 	String msg = "";
-	char curr = ' ';
-	while (1) {
-		if (serial.available()) {
-			curr = serial.read();
-			msg += curr;
-			if (curr == terminationChar) {
-				break;
-			}
+	char curr;
+	while (serial.available()) {
+		curr = serial.read();
+		msg += curr;
+		if (curr == terminationChar) {
+			break;
 		}
 	}
 	return msg;
